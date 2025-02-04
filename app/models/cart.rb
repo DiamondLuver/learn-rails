06013b989195ecd8c7e6476f72428a48858719c6
DiamondLuver <sreypich.un@student.cadt.edu.kh>
2025-02-04 16:59:44 +0700

    Add status enum to Cart model and implement checkout_order and get_shopping_carts methods

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,6 +3,9 @@ class Cart < ApplicationRecord
   has_many :shopping_carts
   has_many :products, through: :shopping_carts
 
+  enum status: { pending: 0, checkout: 1, payment: 2, completed: 3 }
+
+
   def add_product(product, quantity)
     item = shopping_carts.find_or_initialize_by(product: product)
     if item.quantity + quantity > product.quantity
@@ -20,5 +23,21 @@ class Cart < ApplicationRecord
   def remove_product(product)
     item = shopping_carts.find_by(product: product)
     item.destroy
+  end
+
+  def checkout_order
+    shopping_carts.each do |item|
+      product = item.product
+      product.quantity -= item.quantity
+      product.save
+      item.is_completed = true
+    end
+    self.status = :checkout
+    update(status: :checkout)
+    # shopping_carts.destroy_all
+  end
+
+  def get_shopping_carts
+    shopping_carts.where(is_completed: false) 
   end
 end
