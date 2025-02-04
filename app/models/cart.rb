36294abc164a2084e76432a1aef67a683a2ce69e
DiamondLuver <sreypich.un@student.cadt.edu.kh>
2025-02-04 15:16:28 +0700

    Update add_product method to handle quantity and prevent over-adding products

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,12 +3,18 @@ class Cart < ApplicationRecord
   has_many :shopping_carts
   has_many :products, through: :shopping_carts
 
-  def add_product(product)
+  def add_product(product, quantity)
     item = shopping_carts.find_or_initialize_by(product: product)
+    if item.quantity + quantity > product.quantity
+      return false
+    end
+    item.quantity += quantity
     item.save
   end
+
   def total_price
-    shopping_carts.sum(&:total_price)
+    # shopping_carts.sum { |item| item.quantity * item.product.price }
+    # shopping_carts.sum(&:products.price)
   end
 
   def remove_product(product)
