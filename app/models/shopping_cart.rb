cbf8a7fa254483da59b2479d1eabade5560fd22b
DiamondLuver <sreypich.un@student.cadt.edu.kh>
2025-02-04 15:16:34 +0700

    Add methods to calculate total price and quantity in shopping cart

--- app/models/shopping_cart.rb
+++ app/models/shopping_cart.rb
@@ -2,4 +2,10 @@ class ShoppingCart < ApplicationRecord
   belongs_to :cart
   belongs_to :product
 
+  def total_price
+    cart.shopping_carts.includes(:product).sum { |item| item.quantity * item.product.price }
+  end
+  def total_quantity
+    cart.shopping_carts.sum(:quantity)
+  end
 end
